56c35647cbb83b970a5844419621401795f36923
Derrick Gillespie <derrickgillespiewv@gmail.com>
2017-07-21 13:15:56 -0400

    Non-Slice Update

--- rand_name.rb
+++ rand_name.rb
@@ -6,8 +6,50 @@ def names_pair(names)
 		extra = extra[0]
 		sets[0] << extra
 	end 
-	p sets
+	# p sets
 	sets
 end
 
-# names_pair (["a","b","c","d","e"])
+def names_two(names)
+		little_array = []
+		sets = names.shuffle
+	if names.length % 2 == 0
+		a = names.length/2
+		a.times do 
+		sets_1 = sets.pop(2).to_a
+		little_array << sets_1
+		set_1 = 0
+	end
+
+	else 
+		extra = sets.pop
+		b = names.length/2
+		b.times do 
+		sets_1 = sets.pop(2).to_a
+		little_array << sets_1
+		set_1 = 0
+	end
+	little_array[0] << extra
+end
+
+p little_array
+end
+
+# def name_two(names)
+# sets = names.map{|s| s.split(',')}
+# sets.to_a
+# p sets
+# end
+
+
+# def name_two(names)
+# sets = names.map { |data| data.split(',') }
+# p sets
+# end
+
+# def name_two(names)
+# sets = names.shuffle.combination(2).to_a
+# sets_1 = sets.uniq
+# p sets_1
+# end 
+names_two(["a","b","c","d","e"])
--- rand_name_test.rb
+++ rand_name_test.rb
@@ -21,8 +21,28 @@ class TestClose_BNC < Minitest::Test
 		assert_equal(Array, results.class)
 	end
 
+	def test_names_count
+		results = names_pair(["Katie Collins","Aaron Camersi","Derrick Gillispie","Andrew Farley","Adrian Sauls","John Murphy","Steven Miller","Victoria Frame","Stephanie Frame","Chris Phelps","Derick West","Ty Cook","Robert Caldwell","Marcus Folks","Tyler Richards","Mike Handy"])
+		assert_equal(8, results.count)
+	end
+
 	def test_names_out_extra
 		results = names_pair(["Katie Collins","Aaron Camersi","Derrick Gillispie","Andrew Farley","Adrian Sauls","John Murphy","Steven Miller","Victoria Frame","Stephanie Frame","Chris Phelps","Derick West","Ty Cook","Robert Caldwell","Marcus Folks","Tyler Richards","Mike Handy","EXTRA-NAME"])
 		assert_equal(Array, results.class)
+	end
+
+	def test_names_out_first_is_3
+		results = names_pair(["Katie Collins","Aaron Camersi","Derrick Gillispie","Andrew Farley","Adrian Sauls","John Murphy","Steven Miller","Victoria Frame","Stephanie Frame","Chris Phelps","Derick West","Ty Cook","Robert Caldwell","Marcus Folks","Tyler Richards","Mike Handy","EXTRA-NAME"])
+		assert_equal(3, results[0].count)
+	end
+
+	def test_names_two_count
+		results = names_two(["Katie Collins","Aaron Camersi","Derrick Gillispie","Andrew Farley","Adrian Sauls","John Murphy","Steven Miller","Victoria Frame","Stephanie Frame","Chris Phelps","Derick West","Ty Cook","Robert Caldwell","Marcus Folks","Tyler Richards","Mike Handy"])
+		assert_equal(8, results.count)
+	end
+
+	def test_names_two_count_odd
+		results = names_two(["Katie Collins","Aaron Camersi","Derrick Gillispie","Andrew Farley","Adrian Sauls","John Murphy","Steven Miller","Victoria Frame","Stephanie Frame","Chris Phelps","Derick West","Ty Cook","Robert Caldwell","Marcus Folks","Tyler Richards","Mike Handy","EXTRA-Name"])
+		assert_equal(8, results.count)
 	end
 end
